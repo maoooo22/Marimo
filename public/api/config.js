818804cc34e57869147b0877376ad57d1583afeb
maoooo22 <justmarimo@gmail.com>
2025-09-01 10:56:16 +0700

    Create config.js

---
+++ public/api/config.js
@@ -0,0 +1,6 @@
+// File ini berfungsi sebagai pengganti .env untuk menyimpan kunci-kunci penting.
+// Pastikan file ini berada di folder yang sama dengan index.html
+
+// Ganti dengan URL dan Kunci Anon dari proyek Supabase kamu.
+const SUPABASE_URL = 'https://zvxkltvmrfxgrlrrekob.supabase.co'; 
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp2eGtsdHZtcmZ4Z3JscnJla29iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY2NjAyMTQsImV4cCI6MjA3MjIzNjIxNH0.dR_TGhTKJYOMtNek_N7BXc2fXhLY9EH9e1krrflSG3Q';
